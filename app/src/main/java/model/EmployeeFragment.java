e9aae86f43c89c0fdc572ae8b4844ecd1b4425cf
Sara Mazloumi <mz.sara29@gmail.com>
2019-04-13 15:05:21 -0400

    some simple changes

--- app/src/main/java/model/EmployeeFragment.java
+++ app/src/main/java/model/EmployeeFragment.java
@@ -15,6 +15,7 @@ import java.util.StringTokenizer;
 
 public class EmployeeFragment extends android.app.Fragment {
 
+
     //set the layout
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,40 +29,42 @@ public class EmployeeFragment extends android.app.Fragment {
 
         Employee employee = (Employee) getArguments().getSerializable("key");
            if (employee instanceof FullTime){
-               TextView textViewFId = (TextView)getActivity().findViewById(R.id.editTextId);
-               TextView textViewFName = (TextView)getActivity().findViewById(R.id.editTextName);
-               TextView textViewFJob = (TextView)getActivity().findViewById(R.id.editTextJob);
-               TextView textViewFStartDate = (TextView)getActivity().findViewById(R.id.editTextStartDate);
-               TextView textViewFSalary = (TextView)getActivity().findViewById(R.id.editTextSalary);
-               TextView textViewEndDate = (TextView)getActivity().findViewById(R.id.editTextEndDate);
-               TextView textViewPerweek = (TextView)getActivity().findViewById(R.id.editTextnhPerWeek);
 
-               textViewFId.setText(employee.getId());
-               textViewFName.setText(employee.name);
-               textViewFJob.setText(employee.job);
-               textViewFStartDate.setText(((FullTime) employee).getFStartDate());
-               textViewFSalary.setText(String.valueOf(((FullTime) employee).getFSalary()));
+               TextView textViewTd = (TextView)getActivity().findViewById(R.id.editTextId);
+               TextView textViewName = (TextView)getActivity().findViewById(R.id.editTextName);
+               TextView textViewJob = (TextView)getActivity().findViewById(R.id.editTextJob);
+               TextView textViewStartDate = (TextView)getActivity().findViewById(R.id.editTextStartDate);
+               TextView textViewSalary = (TextView)getActivity().findViewById(R.id.editTextSalary);
+               TextView textViewEndDate = (TextView)getActivity().findViewById(R.id.editTextEndDate);
+               TextView textViewPerWeek = (TextView)getActivity().findViewById(R.id.editTextnhPerWeek);
+
+               textViewTd.setText(employee.getId());
+               textViewName.setText(employee.name);
+               textViewJob.setText(employee.job);
+               textViewStartDate.setText(((FullTime) employee).getFStartDate());
+               textViewSalary.setText(String.valueOf(((FullTime) employee).getFSalary()));
                textViewEndDate.setVisibility(EditText.INVISIBLE);
-               textViewPerweek.setVisibility(EditText.INVISIBLE);
+               textViewPerWeek.setVisibility(EditText.INVISIBLE);
 
            }else if (employee instanceof Contractor){
-               TextView textViewCId = (TextView)getActivity().findViewById(R.id.editTextId);
-               TextView textViewCName = (TextView)getActivity().findViewById(R.id.editTextName);
-               TextView textViewCJob = (TextView)getActivity().findViewById(R.id.editTextJob);
-               TextView textViewCStartDate = (TextView)getActivity().findViewById(R.id.editTextStartDate);
-               TextView textViewCEndDate = (TextView)getActivity().findViewById(R.id.editTextEndDate);
-               TextView textViewCSalary = (TextView)getActivity().findViewById(R.id.editTextSalary);
-               TextView textViewNHPerWeek = (TextView)getActivity().findViewById(R.id.editTextnhPerWeek);
+               
+               TextView textViewId = (TextView)getActivity().findViewById(R.id.editTextId);
+               TextView textViewName = (TextView)getActivity().findViewById(R.id.editTextName);
+               TextView textViewJob = (TextView)getActivity().findViewById(R.id.editTextJob);
+               TextView textViewStartDate = (TextView)getActivity().findViewById(R.id.editTextStartDate);
+               TextView textViewSalary = (TextView)getActivity().findViewById(R.id.editTextSalary);
+               TextView textViewEndDate = (TextView)getActivity().findViewById(R.id.editTextEndDate);
+               TextView textViewPerWeek = (TextView)getActivity().findViewById(R.id.editTextnhPerWeek);
 
-               textViewCId.setText(employee.getId());
-               textViewCName.setText(employee.getName());
-               textViewCJob.setText(employee.getJob());
-               textViewCStartDate.setText(((Contractor) employee).getcStartDate());
-               textViewCEndDate.setVisibility(EditText.VISIBLE);
-               textViewCEndDate.setText(((Contractor) employee).getEndDate());
-               textViewNHPerWeek.setVisibility(EditText.VISIBLE);
-               textViewCSalary.setText(String.valueOf(((Contractor) employee).getcSalary()) + " Per hour");
-               textViewNHPerWeek.setText(String.valueOf(((Contractor) employee).getNhPerWeek()));
+               textViewId.setText(employee.getId());
+               textViewName.setText(employee.getName());
+               textViewJob.setText(employee.getJob());
+               textViewStartDate.setText(((Contractor) employee).getcStartDate());
+               textViewEndDate.setVisibility(EditText.VISIBLE);
+               textViewEndDate.setText(((Contractor) employee).getEndDate());
+               textViewSalary.setText(String.valueOf(((Contractor) employee).getcSalary()) + " Per hour");
+               textViewPerWeek.setVisibility(EditText.VISIBLE);
+               textViewPerWeek.setText(String.valueOf(((Contractor) employee).getNhPerWeek()));
            }
        }
 }
